180b7691188e7c591d82a743419ce770274ee37a
EdGuz <edguzman2408@gmail.com>
2016-01-26 15:05:27 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,57 +3,45 @@ var router = express.Router();
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var $ = require('jQuery');
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-$(document).ready(function() {
-  $(document).on('click', '#btnSearch', search);
-});
-
-function search(){
+router.get('/scrape', function(req, res){
   
-  //url = 'http://www.imdb.com/title/tt1229340/';
-  
-  var searchValue = $('#inputSearch').val();
-  url = 'http://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=' + searchValue;
+  url = 'http://www.amazon.com/s/ref=nb_sb_ss_c_0_9?url=search-alias%3Daps&field-keywords=hdmi+cable&sprefix=chromecast%2Caps%2C209';
   
   request(url, function(error, response, html){
 		if(!error){
 			var $ = cheerio.load(html);
-      console.log(html);
 
-			var title, release, rating;
-			var json = { title : "", release : "", rating : ""};
-
-			$('.header').filter(function(){
-		        var data = $(this);
-		        title = data.children().first().text();
-		        release = data.children().last().children().text();
-
-		        json.title = title;
-		        json.release = release;
-	        })
-
-	        $('.star-box-giga-star').filter(function(){
-	        	var data = $(this);
-	        	rating = data.text();
-
-	        	json.rating = rating;
-	        })
+			var title, price;
+			var json = { title : "", price : ""};
+      
+      $('.a-size-medium a-color-null s-inline s-access-title a-text-normal').filter(function() {
+        var data = $(this);
+        title = data.text();
+        json.title = title;
+      });
+      
+      $('.a-size-base a-color-price s-price a-text-bold').filter(function() {
+        var data = $(this);
+        price = data.text();
+        json.price = price;
+      });
+		
 		}
 
 		fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
         	console.log('File successfully written! - Check your project directory for the output.json file');
         })
 
-      res.send(JSON.stringify(json));
+      res.json(json);
       })
   
   
-}
+});
 
 module.exports = router;
