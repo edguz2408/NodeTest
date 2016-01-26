1bd1baf389943bc8da6ed14a59f11bc231fec2c1
EdGuz <edguzman2408@gmail.com>
2016-01-26 12:19:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var request = require('request');
 var cheerio = require('cheerio');
 
 var routes = require('./routes/index');
@@ -22,7 +23,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cheerio);
+app.use(request());
+app.use(cheerio());
 
 app.use('/', routes);
 app.use('/users', users);
