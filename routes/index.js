99a9037c4378b3ddc78e032aeb81dd9b19149369
EdGuz <edguzman2408@gmail.com>
2016-01-26 12:29:50 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,49 @@ router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/some', function(req, res){
-  res.send('Hola');
+router.get('/scrape', function(req, res){
+  
+  url = 'http://www.imdb.com/title/tt1229340/';
+  
+  request(function(error, response, html){
+    if(!error){
+      
+      var $ = cheerio.load(html);
+      
+      var title, release, rating;
+      
+      var json = {title : "", release : "", rating : ""};
+      
+      $('.header').filter(function() {
+        
+        var data = $(this);
+        
+        title = data.children().first().text();
+        release = data.children().last().children().text();
+        
+        json.title = title;
+        json.release = release;
+        
+        
+         $('.star-box-giga-star').filter(function() {
+          
+          var data = $(this);
+          
+          rating = data.text();
+          
+          json.rating = rating;
+        });
+        
+      });
+    }
+    
+    fs.writeFile('output.json', JSON.stringify(json, null,4), function(err){
+      console.log('File successfully written! - Check your project directory for the output.json file');
+    });
+    
+  });
+  
+  
 });
 
 module.exports = router;
