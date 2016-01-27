b229d2fb83e3fca7e24d278024231483ebf973ce
EdGuz <edguzman2408@gmail.com>
2016-01-27 10:19:27 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.get('/scrape/:searchParam', function(req, res) {
   urls = ['http://www.amazon.com/s/ref=nb_sb_ss_c_0_9?url=search-alias%3Daps&field-keywords=' + req.params.searchParam,
     'http://www.ebay.com/sch/i.html?_from=R40&_trksid=p2050601.m570.l1311.R1.TR12.TRC2.A0.H0.Xwact.TRS0&_nkw=' + req.params.searchParam
   ];
-  console.log(getData(urls, middleFunc));
+  results = getData(urls, middleFunc));
   res.json(results);
   
 
@@ -27,8 +27,8 @@ router.get('/scrape/:searchParam', function(req, res) {
 
 function middleFunc(objs){
   console.log('callback::' + objs);
-  results = objs;
-  console.log('results callback:: ' + results);
+  return objs;
+  
 }
 
 function getData(urls, callback){
